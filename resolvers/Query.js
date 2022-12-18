b21f942be4c905b629b18b37caa43c15048a1e78
Yi leng Yao <yilengyao@gmail.com>
2022-12-18 15:21:41 -0800

    add filter by tempo_name, movie_ttile, composer_name, instrucment_group, instrument_name

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,9 +1,37 @@
 module.exports = {
-    allTracks: (parent, { sort, sortBy }, { tracks }) => {
-        console.log(sort)
-        console.log(sortBy)
-        console.log(tracks)
+    allTracks: (parent, { movie_title, tempo_name, composer_name, instrument_group, instrument_name, sort, sortBy }, { tracks, composers, track_instruments, instruments }) => {
    // Sort the tracks if sortBy and sort arguments are provided
+   if (composer_name) {
+        tracks = tracks.filter( track => 
+            {
+                return composers.filter(composer => composer.composer_name === composer_name)
+                    .map(composer => composer.composer_id)
+                    .includes(track.composer_id)
+            }
+        )
+   }
+   if (instrument_group) {
+        var track_ids = [... new Set(track_instruments.filter(track_instrument => 
+            instruments.filter( 
+            instrument => instrument.instrument_group === instrument_group)
+            .map(instrument => instrument.instrument_id).includes(track_instrument.instrument_id))
+            .map(track_instrument => track_instrument.track_id)) ]
+        tracks = tracks.filter(track => track_ids.includes(track.track_id))
+   }
+   if (instrument_name) {
+    var track_ids = [... new Set(track_instruments.filter(track_instrument => 
+        instruments.filter( 
+        instrument => instrument.instrument_name === instrument_name)
+        .map(instrument => instrument.instrument_id).includes(track_instrument.instrument_id))
+        .map(track_instrument => track_instrument.track_id)) ]
+    tracks = tracks.filter(track => track_ids.includes(track.track_id))
+    }
+   if (movie_title) {
+        tracks = tracks.filter(track => track.movie_title === movie_title)
+   }
+   if (tempo_name) {
+        tracks = tracks.filter(track => track.tempo_name === tempo_name)
+   }
     if (sortBy && sort) {
       tracks = tracks.sort((a, b) => {
         
