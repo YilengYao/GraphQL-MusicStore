a324d7704cdd1389f429fc68adc690f0c0ed825f
Yi leng Yao <yilengyao@gmail.com>
2022-12-18 14:14:39 -0800

    sort tracks

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,5 +1,24 @@
 module.exports = {
-    allTracks: (parent, args, { tracks }) => {
-        return tracks
+    allTracks: (parent, { sort, sortBy }, { tracks }) => {
+        console.log(sort)
+        console.log(sortBy)
+        console.log(tracks)
+   // Sort the tracks if sortBy and sort arguments are provided
+    if (sortBy && sort) {
+      tracks = tracks.sort((a, b) => {
+        
+        if (sort === "ASCENDING") {
+          if (a[sortBy] < b[sortBy]) return -1;
+          if (a[sortBy] > b[sortBy]) return 1;
+          return 0;
+        } else {
+          if (a[sortBy] > b[sortBy]) return -1;
+          if (a[sortBy] < b[sortBy]) return 1;
+          return 0;
+        }
+      });        
     }
+    return tracks
+
+}
 }
